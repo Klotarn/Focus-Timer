95a98e84677ec9ac327b587cfb0d4912e61685af
Klotarn <klotarn.swe@gmail.com>
2026-03-03 20:04:25 +0100

    Make timer smoother

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var intervals = [];
 
 function startTimer() {
-    const duration = document.querySelector('#timeInput').valueAsNumber / 1000; // Convert milliseconds to seconds
+    const frequency = 50; // How many times per second the timer updates
+    const duration = document.querySelector('#timeInput').valueAsNumber / (1000 / frequency); // Convert milliseconds to seconds
     const timerTextElement = document.querySelector('.timerText');
     const timerElement = document.querySelector('.timer');
     let timer = duration, minutes, seconds;
 
     const interval = setInterval(() => {
-        minutes = Math.floor(timer / 60);
-        seconds = timer % 60;
+        minutes = Math.floor(timer / (60 * frequency));
+        seconds = Math.floor(timer % (60 * frequency) / frequency);
         timerTextElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
         timerElement.style.background = `conic-gradient(#f0f0f0 ${(duration - timer) / duration * 100}%, #ff0000 ${(duration - timer) / duration * 100}%)`;
 
@@ -16,7 +17,7 @@ function startTimer() {
             clearInterval(interval);
             timerTextElement.textContent = "Time's up!";
         }
-    }, 1000);
+    }, 1000 / frequency);
 
     intervals.push(interval);
 }
